1c17126becd3c55610eb4c769aba65de7c0ab488
Ubuntu <ubuntu@ip-172-31-87-22.ec2.internal>
2025-08-31 14:58:03 +0000

    s3 bucket versioning in module

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -2,3 +2,11 @@ resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
   tags   = var.tags
 }
+# Enable versioning
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
